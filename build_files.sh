c9f9af5a66597151a183d1a16eb8e1f1a9538cb2
abdul1mug-png <abdul1mug@gmail.com>
2026-03-11 08:21:29 +0500

    Fix pip install flag

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uv pip install -r requirements.txt --system
+pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 python3 manage.py populate_tests
